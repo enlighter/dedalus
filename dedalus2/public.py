c603669e9a23ab945205f1f3affd4ee3551165ed
Keaton J. Burns <keaton.burns@gmail.com>
2014-06-16 16:34:14 -0400

    Modify logging filehandler so config filename can include folders, i.e. log/dedalus_p0.log

---
+++ dedalus2/public.py
@@ -0,0 +1,16 @@
+"""
+Interface for tools typically accessed for solving a problem.
+
+"""
+
+# Import custom logging to setup rootlogger
+from .tools import logging
+
+from .data import future as _future
+from .data.domain import Domain
+from .data import operators
+from .pde.basis import Chebyshev, Fourier, Compound
+from .pde import solvers
+from .pde.problems import ParsedProblem, MatrixProblem
+from .pde import timesteppers
+
