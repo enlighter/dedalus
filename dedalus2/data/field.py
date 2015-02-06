27e9b783f618be92ec0c1d0f81d2932d9507e0b5
maxhutch <maxhutch@gmail.com>
2015-02-06 14:17:59 +0000

    Typo in doc notebook

---
+++ dedalus2/data/field.py
@@ -0,0 +1,247 @@
+"""
+Class for data fields.
+
+"""
+
+from functools import partial
+import numpy as np
+from scipy import sparse
+from scipy.sparse import linalg as splinalg
+import weakref
+
+from .future import Future
+from ..tools.config import config
+
+# Load config options
+permc_spec = config['linear algebra']['permc_spec']
+use_umfpack = config['linear algebra'].getboolean('use_umfpack')
+
+
+class Field(Future):
+    """
+    Scalar field over a domain.
+
+    Parameters
+    ----------
+    domain : domain object
+        Problem domain
+    name : str, optional
+        Field name (default: Python object id)
+
+    Attributes
+    ----------
+    layout : layout object
+        Current layout of field
+    data : ndarray
+        View of internal buffer in current layout
+
+    """
+
+    def __init__(self, domain, name=None, constant=None):
+
+        # Initial attributes
+        self.name = name
+
+        # Weak-reference domain to allow cyclic garbage collection
+        self._domain_weak_ref = weakref.ref(domain)
+
+        # Increment domain field count
+        domain._field_count += 1
+
+        # Allocate data buffer
+        self._buffer = domain.distributor.create_buffer()
+
+        # Set initial layout (property hook sets data view)
+        self.layout = self.domain.distributor.coeff_layout
+
+        # Default to non-constant
+        if constant is None:
+            constant = np.array([False] * domain.dim)
+        self.constant = constant
+
+    def clean(self):
+        """Revert field to state at instantiation."""
+
+        self.layout = self.domain.distributor.coeff_layout
+        self.constant[:] = False
+        self.data.fill(0.)
+        self.name = None
+
+    @property
+    def layout(self):
+        return self._layout
+
+    @layout.setter
+    def layout(self, layout):
+        self._layout = layout
+        self.data = layout.view_data(self._buffer)
+
+    @property
+    def domain(self):
+        return self._domain_weak_ref()
+
+    def __del__(self):
+        """Intercept deallocation to cache unused fields in domain."""
+
+        # Check that domain is still instantiated
+        if self.domain:
+            self.domain._collect_field(self)
+
+    def __repr__(self):
+        return '<Field %i>' %id(self)
+
+    def __str__(self):
+        if self.name:
+            return self.name
+        else:
+            return self.__repr__()
+
+    def __getitem__(self, layout):
+        """Return data viewed in specified layout."""
+
+        self.require_layout(layout)
+
+        return self.data
+
+    def __setitem__(self, layout, data):
+        """Set data viewed in a specified layout."""
+
+        layout = self.domain.distributor.get_layout_object(layout)
+        self.layout = layout
+        np.copyto(self.data, data)
+
+    def require_layout(self, layout):
+        """Change to specified layout."""
+
+        layout = self.domain.distributor.get_layout_object(layout)
+
+        # Transform to specified layout
+        if self.layout.index < layout.index:
+            while self.layout.index < layout.index:
+                self.towards_grid_space()
+        elif self.layout.index > layout.index:
+            while self.layout.index > layout.index:
+                self.towards_coeff_space()
+
+    def towards_grid_space(self):
+        """Change to next layout towards grid space."""
+
+        self.domain.distributor.increment_layout(self)
+
+    def towards_coeff_space(self):
+        """Change to next layout towards coefficient space."""
+
+        self.domain.distributor.decrement_layout(self)
+
+    def require_grid_space(self, axis=None):
+        """Require one axis (default: all axes) to be in grid space."""
+
+        if axis is None:
+            while not all(self.layout.grid_space):
+                self.towards_grid_space()
+        else:
+            while not self.layout.grid_space[axis]:
+                self.towards_grid_space()
+
+    def require_coeff_space(self, axis=None):
+        """Require one axis (default: all axes) to be in coefficient space."""
+
+        if axis is None:
+            while any(self.layout.grid_space):
+                self.towards_coeff_space()
+        else:
+            while self.layout.grid_space[axis]:
+                self.towards_coeff_space()
+
+    def require_local(self, axis):
+        """Require an axis to be local."""
+
+        # Move towards transform path, since the surrounding layouts are local
+        if self.layout.grid_space[axis]:
+            while not self.layout.local[axis]:
+                self.towards_coeff_space()
+        else:
+            while not self.layout.local[axis]:
+                self.towards_grid_space()
+
+    def differentiate(self, basis, out=None):
+        """Differentiate field along one basis."""
+
+        # Use differentiation operator
+        basis = self.domain.get_basis_object(basis)
+        axis = self.domain.bases.index(basis)
+        diff_op = self.domain.diff_ops[axis]
+        return diff_op(self, out=out).evaluate()
+
+    def integrate(self, *bases, out=None):
+        """Integrate field over bases."""
+
+        # Use integration operator
+        from .operators import Integrate
+        return Integrate(self, *bases, out=out).evaluate()
+
+    def antidifferentiate(self, basis, bc, out=None):
+        """
+        Antidifferentiate field by setting up a simple linear BVP.
+
+        Parameters
+        ----------
+        basis : basis-like
+            Basis to antidifferentiate along
+        bc : (str, object) tuple
+            Boundary conditions as (functional, value) tuple.
+            `functional` is a string, e.g. "left", "right", "int"
+            `value` is a field or scalar
+        out : field, optional
+            Output field
+
+        """
+
+        # References
+        basis = self.domain.get_basis_object(basis)
+        domain = self.domain
+        bc_type, bc_val = bc
+
+        # Only solve along last basis
+        if basis is not domain.bases[-1]:
+            raise NotImplementedError()
+
+        # Convert BC value to field
+        if np.isscalar(bc_val):
+            bc_val = domain.new_field()
+            bc_val['g'] = bc[1]
+        elif not isinstance(bc_val, Field):
+            raise TypeError("bc_val must be field or scalar")
+
+        # Build LHS matrix
+        size = basis.coeff_size
+        dtype = basis.coeff_dtype
+        Pre = basis.Pre
+        Diff = basis.Diff
+        BC = getattr(basis, bc_type.capitalize())
+        try:
+            Lm = basis.Match
+        except AttributeError:
+            Lm = sparse.csr_matrix((size, size), dtype=dtype)
+
+        # Find rows to replace
+        BC_rows = BC.nonzero()[0]
+        Lm_rows = Lm.nonzero()[0]
+        F = sparse.identity(basis.coeff_size, dtype=basis.coeff_dtype, format='dok')
+        for i in set().union(BC_rows, Lm_rows):
+            F[i, i] = 0
+        G = F*Pre
+        LHS = G*Diff + BC + Lm
+
+        if not out:
+            out = self.domain.new_field()
+        out_c = out['c']
+        f_c = self['c']
+        bc_c = bc_val['c']
+
+        # Solve for each pencil
+        for p in np.ndindex(out_c.shape[:-1]):
+            rhs = G*f_c[p] + BC*bc_c[p]
+            out_c[p] = splinalg.spsolve(LHS, rhs, use_umfpack=use_umfpack, permc_spec=permc_spec)
+
+        return out
---
+++ dedalus2/tools/post.py
@@ -0,0 +1,222 @@
+"""
+Post-processing helpers.
+
+"""
+
+import pathlib
+import h5py
+import numpy as np
+from mpi4py import MPI
+
+from ..tools.general import natural_sort
+
+import logging
+logger = logging.getLogger(__name__.split('.')[-1])
+
+MPI_RANK = MPI.COMM_WORLD.rank
+MPI_SIZE = MPI.COMM_WORLD.size
+
+
+def default_function(file_path, start, count):
+    print('Rank %i: file path = %s, start = %i, count = %i' %(MPI_RANK, filename, start, count))
+
+
+def visit(file_paths, function=default_function, **kw):
+    """Apply function to files/writes assigned to MPI process.
+
+    Parameters
+    ----------
+    file_paths : list of str or pathlib.Paths
+        List of file paths
+    function : function(file, start, count, **kw)
+        A function on an HDF5 file, start index, and count.
+
+    Other keyword arguments passed on to `function`
+
+    """
+
+    file_paths = natural_sort(str(fp) for fp in file_paths)
+    function_calls = zip(file_paths, *get_assigned_writes(file_paths))
+    for file_path, start, count in function_calls:
+        if count:
+            function(file_path, start, count, **kw)
+
+
+def get_assigned_writes(file_paths):
+    """
+    Distribute files/writes matching a pattern between MPI processes.
+
+    Parameters
+    ----------
+    file_paths : list of str or pathlib.Path
+        List of file paths
+
+    """
+
+    file_paths = natural_sort(str(fp) for fp in file_paths)
+    # Distribute all writes in blocks
+    writes = get_all_writes(file_paths)
+    block = int(np.ceil(sum(writes) / MPI_SIZE))
+    proc_start = MPI_RANK * block
+    # Find file start/end indices
+    writes = np.array(writes)
+    file_ends = np.cumsum(writes)
+    file_starts = file_ends - writes
+    # Find proc start indices and counts for each file
+    starts = np.clip(proc_start, a_min=file_starts, a_max=file_ends)
+    counts = np.clip(proc_start+block, a_min=file_starts, a_max=file_ends) - starts
+
+    return starts-file_starts, counts
+
+
+def get_all_writes(file_paths):
+    """
+    Find all files/writes matching a pattern.
+
+    Parameters
+    ----------
+    file_paths : list of str or pathlib.Path
+        List of file paths
+
+    """
+
+    file_paths = natural_sort(str(fp) for fp in file_paths)
+    # Get write numbers
+    writes = list()
+    for file_path in file_paths:
+        with h5py.File(str(file_path), mode='r') as file:
+            writes.append(file.attrs['writes'])
+
+    return writes
+
+
+def merge_analysis(base_path):
+    """
+    Merge distributed output files from a FileHandler.
+    MPI parallelized up to number of distributed output files.
+
+    Parameters
+    ----------
+    base_path : str or pathlib.Path
+        Base path of FileHandler output
+
+    """
+
+    base_path = pathlib.Path(base_path)
+    logger.info("Merging files from %s" %base_path)
+
+    base_stem = base_path.stem
+    folder_paths = base_path.glob("%s_f*" %base_stem)
+    folder_paths = filter(lambda path: path.is_dir(), folder_paths)
+    folder_paths = natural_sort(folder_paths)
+    for folder_path in folder_paths[MPI_RANK::MPI_SIZE]:
+        merge_folder(folder_path)
+
+
+def merge_folder(folder_path):
+    """
+    Merge folder containing a distributed output file.
+
+    Parameters
+    ----------
+    folder_path : str of pathlib.Path
+        Path to folder containing a distributed output file
+
+    """
+
+    folder_path = pathlib.Path(folder_path)
+    logger.info("Merging folder %s" %folder_path)
+
+    folder_stem = folder_path.stem
+    proc_paths = folder_path.glob("%s_p*.h5" %folder_stem)
+    proc_paths = natural_sort(proc_paths)
+    joint_path = folder_path.parent.joinpath("%s.h5" %folder_stem)
+
+    # Create joint file, overwriting if it already exists
+    with h5py.File(str(joint_path), mode='w') as joint_file:
+        # Setup joint file based on first process file (arbitrary)
+        merge_setup(joint_file, proc_paths[0])
+        # Merge data from all process files
+        for proc_path in proc_paths:
+            merge_data(joint_file, proc_path)
+
+
+def merge_setup(joint_file, proc_path):
+    """
+    Merge HDF5 setup from part of a distributed output file into a joint file.
+
+    Parameters
+    ----------
+    joint_file : HDF5 file
+        Joint file
+    proc_path : str or pathlib.Path
+        Path to part of a distributed output file
+
+    """
+
+    proc_path = pathlib.Path(proc_path)
+    logger.info("Merging setup from %s" %proc_path)
+
+    with h5py.File(str(proc_path), mode='r') as proc_file:
+        # File metadata
+        joint_file.attrs['file_number'] = proc_file.attrs['file_number']
+        joint_file.attrs['handler_name'] = proc_file.attrs['handler_name']
+        try:
+            joint_file.attrs['writes'] = writes = proc_file.attrs['writes']
+        except KeyError:
+            joint_file.attrs['writes'] = writes = len(proc_file['scales']['write_number'])
+        # Copy scales (distributed files all have global scales)
+        proc_file.copy('scales', joint_file)
+        # Tasks
+        joint_tasks = joint_file.create_group('tasks')
+        proc_tasks = proc_file['tasks']
+        for taskname in proc_tasks:
+            # Setup dataset with automatic chunking
+            proc_dset = proc_tasks[taskname]
+            spatial_shape = proc_dset.attrs['global_shape']
+            joint_shape = (writes,) + tuple(spatial_shape)
+            joint_dset = joint_tasks.create_dataset(name=proc_dset.name,
+                                                    shape=joint_shape,
+                                                    dtype=proc_dset.dtype,
+                                                    chunks=True)
+            # Dataset metadata
+            joint_dset.attrs['task_number'] = proc_dset.attrs['task_number']
+            joint_dset.attrs['constant'] = proc_dset.attrs['constant']
+            joint_dset.attrs['grid_space'] = proc_dset.attrs['grid_space']
+            # Dimension scales
+            for i, proc_dim in enumerate(proc_dset.dims):
+                joint_dset.dims[i].label = proc_dim.label
+                for scalename in proc_dim:
+                    scale = joint_file['scales'][scalename]
+                    joint_dset.dims.create_scale(scale, scalename)
+                    joint_dset.dims[i].attach_scale(scale)
+
+
+def merge_data(joint_file, proc_path):
+    """
+    Merge data from part of a distributed output file into a joint file.
+
+    Parameters
+    ----------
+    joint_file : HDF5 file
+        Joint file
+    proc_path : str or pathlib.Path
+        Path to part of a distributed output file
+
+    """
+
+    proc_path = pathlib.Path(proc_path)
+    logger.info("Merging data from %s" %proc_path)
+
+    with h5py.File(str(proc_path), mode='r') as proc_file:
+        for taskname in proc_file['tasks']:
+            joint_dset = joint_file['tasks'][taskname]
+            proc_dset = proc_file['tasks'][taskname]
+            # Merge across spatial distribution
+            start = proc_dset.attrs['start']
+            count = proc_dset.attrs['count']
+            spatial_slices = tuple(slice(s, s+c) for (s,c) in zip(start, count))
+            # Merge maintains same set of writes
+            slices = (slice(None),) + spatial_slices
+            joint_dset[slices] = proc_dset[:]
+
